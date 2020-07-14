6617f968e2e1a237ab9c9bb099795f3ebc643467
ausaafnabi <nabiausaaf@gmail.com>
2020-07-14 12:31:55 +0530

    [fixed] Component malfunctioning| removed log table

--- app/mdmain.py
+++ app/mdmain.py
@@ -6,29 +6,24 @@ from kivy.uix.floatlayout import FloatLayout
 from kivymd.uix.tab import MDTabsBase
 #from Components import ControlPanel
 from controlpanel import ControlPanel
+from graph import Grapher
 Window.minimum_width,Window.minimum_height = (700,500)
 
-#============to remove when segregate grapher func==================
-from matplotlib import style
-from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
-import matplotlib.pyplot as plt
-style.use("dark_background")
-#=================================================================
-KV = '''
 
-'''
-plt.plot([1,23,2,5])
-plt.ylabel('some number')
+Builder.load_file("UI/Components/controlpanel.kv")
+Builder.load_file("UI/Components/logger.kv")
+Builder.load_file("UI/Components/graphproperties.kv")
+
 
 class Tab(FloatLayout,MDTabsBase):
 	pass
+
 class Graph(BoxLayout):
 	pass
 
-class Grapher(BoxLayout):
-	def __init__(self,**kwargs):
-		super().__init__(**kwargs)
-		self.add_widget(FigureCanvasKivyAgg(plt.gcf()))
+class Sidebar(BoxLayout):
+	pass
+
 
 class LogTable(BoxLayout):
 	pass
@@ -39,11 +34,11 @@ class GraphProperties(BoxLayout):
 class Logger(BoxLayout):
 	pass 
 
-class TestUI(MDApp):
+class Main(MDApp):
 	def build(self):
 		self.theme_cls.theme_style = "Dark"
 		return Builder.load_file("UI/mdmain.kv")
 
-TestUI().run()
+Main().run()
 
 	
